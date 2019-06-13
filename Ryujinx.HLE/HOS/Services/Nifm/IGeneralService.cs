4343f8eda78c2cf96d558cef6eda0186966c18f4
Thomas Guillemard <me@thog.eu>
2019-06-13 12:46:57 +0200

    nifm: Implement IGeneralService::GetCurrentIpConfigInfo

--- Ryujinx.HLE/HOS/Services/Nifm/IGeneralService.cs
+++ Ryujinx.HLE/HOS/Services/Nifm/IGeneralService.cs
@@ -21,8 +21,9 @@ namespace Ryujinx.HLE.HOS.Services.Nifm
         {
             _commands = new Dictionary<int, ServiceProcessRequest>
             {
-                { 4, CreateRequest        },
-                { 12, GetCurrentIpAddress }
+                { 4, CreateRequest           },
+                { 12, GetCurrentIpAddress    },
+                { 15, GetCurrentIpConfigInfo }
             };
         }
 
@@ -51,6 +52,88 @@ namespace Ryujinx.HLE.HOS.Services.Nifm
             context.ResponseData.Write(BitConverter.ToUInt32(address.GetAddressBytes()));
 
             Logger.PrintInfo(LogClass.ServiceNifm, $"Console's local IP is \"{address}\".");
+
+            return 0;
+        }
+
+        public long GetCurrentIpConfigInfo(ServiceCtx context)
+        {
+            if (!NetworkInterface.GetIsNetworkAvailable())
+            {
+                return MakeError(ErrorModule.Nifm, NifmErr.NoInternetConnection);
+            }
+
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress address = host.AddressList.FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork);
+
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            IPInterfaceProperties targetProperties = null;
+            UnicastIPAddressInformation targetAddress = null;
+
+            foreach (NetworkInterface adapter in interfaces)
+            {
+                if (adapter.NetworkInterfaceType == NetworkInterfaceType.Loopback)
+                {
+                    continue;
+                }
+                if (adapter.Supports(NetworkInterfaceComponent.IPv4))
+                {
+                    IPInterfaceProperties properties = adapter.GetIPProperties();
+
+                    UnicastIPAddressInformationCollection unicastCollection = properties.UnicastAddresses;
+                    if (unicastCollection != null && unicastCollection.Count > 0 )
+                    {
+                        foreach (UnicastIPAddressInformation info in unicastCollection)
+                        {
+                            if (info.Address == address && properties.GatewayAddresses.Count > 0
+                                && properties.DnsAddresses.Count > 1)
+                            {
+                                targetProperties = properties;
+                                targetAddress = info;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (targetProperties != null)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (targetProperties != null)
+            {
+                Logger.PrintInfo(LogClass.ServiceNifm, $"Console's local IP is \"{address}\".");
+
+                // Unknown
+                context.ResponseData.Write(false);
+
+                // Ip address
+                context.ResponseData.Write(BitConverter.ToUInt32(targetAddress.Address.GetAddressBytes()));
+
+                // Sub network mask
+                context.ResponseData.Write(BitConverter.ToUInt32(targetAddress.IPv4Mask.GetAddressBytes()));
+
+                // Gateway address
+                context.ResponseData.Write(BitConverter.ToUInt32(targetProperties.GatewayAddresses[0].Address.GetAddressBytes()));
+
+
+
+                // Unknown
+                context.ResponseData.Write(false);
+
+                // Primary DNS address
+                context.ResponseData.Write(BitConverter.ToUInt32(targetProperties.DnsAddresses[0].GetAddressBytes()));
+
+                // Secondary DNS address
+                context.ResponseData.Write(BitConverter.ToUInt32(targetProperties.DnsAddresses[1].GetAddressBytes()));
+            }
+            else
+            {
+                return MakeError(ErrorModule.Nifm, NifmErr.NoInternetConnection);
+            }
 
             return 0;
         }
